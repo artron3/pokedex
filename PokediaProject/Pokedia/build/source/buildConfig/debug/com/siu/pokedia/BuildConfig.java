572afbf6913f54b66da25ba1e9b4a46bfff8965e
artron3 <149784@supinfo.com>
2013-06-23 01:16:52 +0200

    Basic HUD

---
+++ PokediaProject/Pokedia/build/source/buildConfig/debug/com/siu/pokedia/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.siu.pokedia;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
---
+++ PokediaProject/Pokedia/src/main/java/com/siu/pokedia/TeamBuilderFragment.java
@@ -0,0 +1,68 @@
+package com.siu.pokedia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.siu.pokedia.dummy.DummyContent;
+
+/**
+ * An activity representing a single Item detail screen. This
+ * activity is only used on handset devices. On tablet-size devices,
+ * item details are presented side-by-side with a list of items
+ * in a {@link com.siu.pokedia.ItemListActivity}.
+ * <p>
+ * This activity is mostly just a 'shell' activity containing nothing
+ * more than a {@link com.siu.pokedia.ItemDetailFragment}.
+ */
+public class TeamBuilderFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public TeamBuilderFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.content);
+        }
+
+        return rootView;
+    }
+}
